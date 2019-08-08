ab1f06200896e5994269ef8ff67c6ed4aaf881dc
Skryagin <37326491+Skryagin@users.noreply.github.com>
2019-08-08 10:38:47 +0300

    Update Logo.jsx

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -5,7 +5,7 @@ import './Logo.css';
 const Logo = () => {
     return (
         <a href='/' className="logo">
-            <h1 className="title">Музыкальный плеер</h1>
+            <h1 className="title">Music player</h1>
         </a>
     )
 };
